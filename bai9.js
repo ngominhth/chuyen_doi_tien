0195aed6aa16dd99e802fc97322e6aa12fbfc86a
ngominh <ngominhth87@gmail.com>
2021-10-11 21:12:15 +0700

    bai tap

--- bai9.js
+++ bai9.js
@@ -3,18 +3,32 @@ function doi_tien()
 {
     let thanhtien;
     let tien_dola=document.getElementById("tien_dola").value;
-    var tien_candoi=document.getElementById("tien_candoi").value;
-    if (tien_candoi=="usd")
+    let tien_candoi=document.getElementById("tien_candoi").value;
+    let tiendadoi=document.getElementById("tiendadoi").value;
+    
+    if (tien_candoi=="usd" && tiendadoi=="vnd")
     {
-       thanhtien=23000;
+       thanhtien=23000*tien_dola+" VNĐ";
     }
-    else if (tien_candoi=="tien_nhat")
+    else if (tien_candoi=="tien_nhat" && tiendadoi=="vnd")
     {
-        thanhtien=206;
+        thanhtien=206*tien_dola+ " VNĐ";
+    }
+    else if (tien_candoi=="vnd" && tiendadoi=="vnd")
+    {
+        thanhtien=1*tien_dola+ " VNĐ";
+    }
+    else if (tien_candoi=="vnd" && tiendadoi=="usd")
+    {
+        thanhtien=tien_dola/23000 +" USD";
+    }
+    else if (tien_candoi=="usd" && tiendadoi=="usd")
+    {
+        thanhtien=tien_dola*1 +" USD";
     }
 
-    let doitien=thanhtien*tien_dola;
-    document.getElementById("hien_thi").innerHTML="Thành tiền: " + doitien + " vnđ";
+    
+    document.getElementById("hien_thi").innerHTML="Thành tiền: " + thanhtien;
 }
 
 
